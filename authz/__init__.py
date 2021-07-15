cd9423d243f1c8c5138aba8c5a70b81b98466b95
Soheil <soheil.mahdavi1362@gmail.com>
2021-07-15 21:26:07 +0200

    Create application structure

--- authz/__init__.py
+++ authz/__init__.py
@@ -1,8 +1,12 @@
 from flask import Flask
+from flask_restful import Api
 from authz.config import Config
+
+api = Api()
 
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)  # Load config from env variables
+    api.init_app(app)
     return app
