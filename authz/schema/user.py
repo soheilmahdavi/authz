2b0ea0ee4270d84dfe73efd797d6da84fdb398c9
Soheil <soheil.mahdavi1362@gmail.com>
2021-07-21 21:35:39 +0200

    adding users schema

--- authz/schema/user.py
+++ authz/schema/user.py
@@ -4,11 +4,17 @@ from authz.model import User
 
 class UserSchema(ma.SQLAlchemySchema):
     class Meta:
+        '''
+            this class is for Marshmalow.
+            Fetch mode vie SQLAlchemy
+            It helps for serialization and deserialization
+            It helps for Validation
+        '''
         model = User
 
     id = ma.auto_field(dump_only=True)  # dump_only means Only readable
     username = ma.auto_field()
-    password = ma.auto_field(load_only=True)
+    password = ma.auto_field(load_only=True)  # Only is writable
     role = ma.auto_field()
     register_at = ma.auto_field(dump_only=True)
     last_active_at = ma.auto_field(dump_only=True)
